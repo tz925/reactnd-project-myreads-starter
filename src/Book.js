eeed93e553410a708d3f622e93ddd5767d3fef4d
tz925 <sai950925@hotmail.com>
2018-01-24 21:51:19 +0800

    finished Myreads
    home page (Myreads) now is navigated by url. updates the database, dynamically presents the books in right place, and fulfills other requirements.

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function Book(props) {
-  if (props.book) {
-    const book = props.book
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url({book.imageLinks.thumbnail})' }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
-        </div>
-      </li>
-    )
+class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object,
+    onChangeShelf: PropTypes.func,
   }
-  return null
+
+  handleSelect = (event) => {
+    event.preventDefault()
+    if (this.props.onChangeShelf) {
+      this.props.onChangeShelf(this.props.book, event.target.value)
+    }
+  }
+
+  render() {
+    if (this.props.book) {
+      const book = this.props.book
+      return (
+        <li>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url({book.imageLinks.thumbnail})' }}></div>
+              <div className="book-shelf-changer">
+                <select onChange={this.handleSelect} value={book.shelf}>
+                  <option value="none">Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead" >Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+          </div>
+        </li>
+      )
+    }
+    return null
+  }
 }
-Book.propTypes = {
-  book: PropTypes.object
-}
+
+// function Book(props) {
+//   handleSelect = (event) => {
+//     event.preventDefault()
+//     if (this.props.onChangeShelf) {
+//       this.props.onChangeShelf(this.props.book, event.target.value)
+//     }
+//   }
+//   if (props.book) {
+//     const book = props.book
+//     return (
+//       <li>
+//         <div className="book">
+//           <div className="book-top">
+//             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url({book.imageLinks.thumbnail})' }}></div>
+//             <div className="book-shelf-changer">
+//               <select onSelect={this.handleSelect}>
+//                 <option value="none" disabled>Move to...</option>
+//                 <option value="currentlyReading">Currently Reading</option>
+//                 <option value="wantToRead">Want to Read</option>
+//                 <option value="read">Read</option>
+//                 <option value="none">None</option>
+//               </select>
+//             </div>
+//           </div>
+//           <div className="book-title">{book.title}</div>
+//           <div className="book-authors">{book.authors}</div>
+//         </div>
+//       </li>
+//     )
+//   }
+//   return null
+// }
+// Book.propTypes = {
+//   book: PropTypes.object
+// }
 export default Book
---
+++ src/Myreads.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import Book from './Book'
+import { Link } from 'react-router-dom'
+
+class Myreads extends Component {
+
+  render() {
+    const {books, changeShelf} = this.props
+    let currentlyReading
+    let wantToRead
+    let read
+    currentlyReading = books.filter((book) => book.shelf === 'currentlyReading')
+    wantToRead = books.filter((book) => book.shelf === 'wantToRead')
+    read = books.filter((book) => book.shelf === 'read')
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {currentlyReading.map((book) => (
+                    <Book key={book.id} book={book}
+                      onChangeShelf={(book, shelf) => {
+                        changeShelf(book, shelf)
+                      }} />
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {wantToRead.map((book) => (
+                    <Book key={book.id} book={book}
+                      onChangeShelf={(book, shelf) => {
+                        changeShelf(book, shelf)
+                      }}/>
+                  ))}
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {read.map((book) => (
+                    <Book key={book.id} book={book}
+                      onChangeShelf={(book, shelf) => {
+                        changeShelf(book, shelf)
+                      }}/>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to='/search'>Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+}
+export default Myreads
